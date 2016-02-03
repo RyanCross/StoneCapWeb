afc5a8c44f50435b0bc7be33a0b8fe0b812fec2f
Ryan Cross <studiopod.RyanCross@outlook.com>
2016-02-02 20:17:39 -0600

    Initial commit

---
+++ redirects/dbconnect.php
@@ -0,0 +1,22 @@
+<?php
+    /*
+                        //////// NOTICE /////////
+    These variables are used for development in the cloud9 dev. environment only,
+    and must be changed once hosting has moved locations.
+    */
+    
+    $host = "ryancross-nomil-1571803"; 
+    $user = "ryancross";
+    $pass = ""; //no pass
+    $db = "nomil";
+    $port = 3306;
+    
+    //Connect to the database:
+    $db = mysqli_connect($host, $user, $pass, $db, $port) or die(mysql_error()); //connect to database, else generate error onto screen.
+    //$db is now an object of the class mysqli, which represents a connection between PHP and a database.
+    
+    
+   // if ($db != null)
+    //echo "<br><br><h1> Connected to Database. </h1> ";
+    
+?>
---
+++ redirects/regsuccess.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+    <title> Registration| APPNAME </title>
+    <meta name="description" content="Registration success/redirection page, "/>
+    <meta name="author" content="Ryan Cross"/>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"/>
+</head>
+
+<body>
+    <center>
+    <h1>Thank You!</h1>
+    </center>
+    <h3>You have successfully completed the registration form, your request for access has been sent to an administrator. </h3>
+    <h3>An email will be sent to you once you're request has been processed.</h3>
+    <br>
+    <br>
+    <br>
+    <center> <h4> Click <a href="../login.php">here</a> to return to the login page. </h4> </center>
+    
+    
+</body>
+</html>
