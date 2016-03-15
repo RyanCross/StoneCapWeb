e42be136963a7f23e109ee4b654d1d68be2c1e45
Nicholas Cappo <nc2b6@mail.missouri.edu>
2016-03-15 12:40:41 -0500

    Login bugs5

--- login.php
+++ login.php
@@ -31,8 +31,8 @@
 			$salt = intval($res['salt']);
 			$check_hash = sha1($salt . $password);
 
-      print $res['password_hash']
-      print "\n".$check_hash
+      print $res['password_hash'];
+      print "\n".$check_hash;
 
 			if ($check_hash == $res['password_hash']){
 				// Start session and redirect to home.php
