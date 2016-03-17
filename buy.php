1814ab560d67742eaa86d2af0f17722d67af751e
Nicholas Cappo <nc2b6@mail.missouri.edu>
2016-03-17 18:06:23 -0500

    buy.php update2

--- buy.php
+++ buy.php
@@ -12,22 +12,11 @@
 
 <div class="container">
   <div class="page-header">
-    <h1>Buy the game! - <?php $logged_in ?></h1>
+    <h1>Buy the game! - <?php print $logged_in; ?></h1>
   </div>
 
   <?php if ($logged_in) { ?>
   <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>" >
-		<div class="form-group <?php if ($email && $email_error) echo 'has-error'; ?>">
-			<label for="email" class="control-label">Email</label>
-			<input id="email" type="text" name="email" class="form-control" value="<?php echo $email; ?>" required>
-			<?php if ($email && $email_error) echo '<p class="help-block">Sorry, we have no record of this league in our database!</p>'; ?>
-		</div>
-		<div class="form-group <?php if ($password_error) echo 'has-error'; ?>">
-			<label for="password" class="control-label">Password</label>
-			<input id="password" type="password" name="password" class="form-control" required>
-			<?php if ($password_error) echo '<p class="help-block">The password you provided was not correct!</p>'; ?>
-		</div>
-
     <input type="submit" name="submit" class="btn btn-success" value="Buy now!" />
 	</form>
   <?php } else { ?>
