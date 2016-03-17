2e0964bd54d0832352eac106aad2d9366500a5a2
Nicholas Cappo <nc2b6@mail.missouri.edu>
2016-03-17 18:02:18 -0500

    Buy.php update1

--- buy.php
+++ buy.php
@@ -10,9 +10,33 @@
   include_once("util/header.php");
 ?>
 
-<h1>Buy the game! - <?php $email ?></h1>
+<div class="container">
+  <div class="page-header">
+    <h1>Buy the game! - <?php $logged_in ?></h1>
+  </div>
 
-<button class="btn btn-success">Buy now!</button>
+  <?php if ($logged_in) { ?>
+  <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>" >
+		<div class="form-group <?php if ($email && $email_error) echo 'has-error'; ?>">
+			<label for="email" class="control-label">Email</label>
+			<input id="email" type="text" name="email" class="form-control" value="<?php echo $email; ?>" required>
+			<?php if ($email && $email_error) echo '<p class="help-block">Sorry, we have no record of this league in our database!</p>'; ?>
+		</div>
+		<div class="form-group <?php if ($password_error) echo 'has-error'; ?>">
+			<label for="password" class="control-label">Password</label>
+			<input id="password" type="password" name="password" class="form-control" required>
+			<?php if ($password_error) echo '<p class="help-block">The password you provided was not correct!</p>'; ?>
+		</div>
+
+    <input type="submit" name="submit" class="btn btn-success" value="Buy now!" />
+	</form>
+  <?php } else { ?>
+
+    <p>You need to be logged in to buy the game. Log in <a href="login.php">here</a>!</p>
+
+  <?php } ?>
+
+</div>
 
 <?php
   include_once("util/footer.php");
