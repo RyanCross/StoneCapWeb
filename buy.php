2b71e861f765cc2cd7439f65db9897410382f5a1
Nicholas Cappo <nc2b6@mail.missouri.edu>
2016-04-04 18:50:50 -0500

    Buy updates2

--- buy.php
+++ buy.php
@@ -32,11 +32,10 @@
 		<div class="col-xs-12 col-sm-6 col-sm-offset-3">
 
 
-		<h1>Buy the game! - <?php print $logged_in; ?></h1>
+			<h1>Buy Terrachi<small>Account: <?php print $logged_in; ?></small></h1>
 
-		  <?php if ($logged_in) { ?>
-		  <p>All buys:</p>
 		  <?php
+			if ($logged_in) {
 		  $query = "SELECT serial_key,buy_date FROM terrachi_db.game_status WHERE game_owner = '".$logged_in."';";
 		  $res = $db->query($query) or die("Previous purchase check error ". $db->error);
 		  if ($res->num_rows == 0) {
@@ -51,11 +50,13 @@
 					</tr>
 				';
 		    while($row = $res->fetch_assoc()) {
+					$date = new DateTime();
+					$date->setTimestamp($row['buy_date']);
 					print '
 					<tr class="success">
 		      	<td>'.$row['serial_key'].'</td>
-						<td>'.$row['buy_date'].'</td>
-						<td><span class="glyphicon glyphicon-search" aria-hidden="true"></span></td>
+						<td>'.$date->format('g:ia n/j/Y').'</td>
+						<td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
 					<tr>
 					';
 		    }
@@ -64,12 +65,12 @@
 				';
 		  }
 		  ?>
-		  <form class="form-inline" method="POST" action="<?= $_SERVER['PHP_SELF'] ?>" >
+		  <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>" >
 				<div class="form-group <?php if ($invite_error) echo 'has-error'; ?>">
 					<?php if ($invite_error) echo '<p class="help-block">The invite code you provided was not correct!</p>'; ?>
 					<input type="password" class="form-control" name="invite-code" id="invite-code" />
 				</div>
-		    <input type="submit" name="submit" class="btn btn-success" value="Buy now!" />
+		    <input type="submit" name="submit" class="btn btn-success btn-block" value="Buy now!" />
 			</form>
 		  <?php } else { ?>
 
